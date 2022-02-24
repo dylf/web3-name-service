7bb20565975c99114266f5356c4281cbcd58cc08
Dylan Fontaine <dylanfontaine@live.ca>
2022-02-24 07:35:02 -0800

    Add token functionality and tld

--- scripts/run.ts
+++ scripts/run.ts
@@ -18,18 +18,23 @@ const main = async () => {
 
   // We get the contract to deploy
   const DomainContractFactory = await ethers.getContractFactory("Domains");
-  const domain = await DomainContractFactory.deploy();
+  const domain = await DomainContractFactory.deploy("matic");
 
   await domain.deployed();
 
   console.log("Contract deployed to:", domain.address);
   console.log("Contract deployed by:", owner.address);
 
-  const txn = await domain.register("website");
+  const txn = await domain.register("website", {
+    value: ethers.utils.parseEther("0.1"),
+  });
   await txn.wait();
 
   const domainOwner = await domain.getAddress("website");
   console.log("Owner of domain:", domainOwner);
+
+  const balance = await ethers.provider.getBalance(domain.address);
+  console.log("Contract balance:", ethers.utils.formatEther(balance));
 
   // This should error
   const badTxn = await domain
